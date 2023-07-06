55d0e386f1acfa8bf53d345a1ec9b3c438d935a4
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-06 13:14:28 +0700

    Update Layout with Sidebar and Navbar components

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -3,12 +3,27 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+  // isNonMobile is a boolean that is true if the screen is wider than 600px
+  // on mobile, this will be false, and on desktop, it will be true
+  const isNonMobile = useMediaQuery("(mid-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>

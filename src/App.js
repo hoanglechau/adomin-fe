8847a0522a47b03a8fc69994ce3de48039fdf694
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-06 10:43:07 +0700

    Fix the createTheme import in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
 
 function App() {
-  const mode = useSelector(state => state.global.mode);
+  const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (

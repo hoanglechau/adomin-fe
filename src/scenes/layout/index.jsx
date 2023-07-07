ed3b09197bec64e84e892bcfa787eda40f0a755f
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-07 14:06:21 +0700

    Update the Layout

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -4,23 +4,29 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
   // isNonMobile is a boolean that is true if the screen is wider than 600px
   // on mobile, this will be false, and on desktop, it will be true
   const isNonMobile = useMediaQuery("(mid-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+  console.log("🚀 ~ file: index.jsx:16 ~ Layout ~ data:", data);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <Box>
+      <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />

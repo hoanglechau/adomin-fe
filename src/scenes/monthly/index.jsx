f09e6d0bc1b5928d25d68af4b7c29bbdb0dbf4ec
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-23 11:13:42 +0700

    Update the Monthly page

--- src/scenes/monthly/index.jsx
+++ src/scenes/monthly/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box, useTheme } from "@mui/material";
 import Header from "components/Header";
 import { ResponsiveLine } from "@nivo/line";
@@ -36,7 +36,7 @@ const Monthly = () => {
 
     const formattedData = [totalSalesLine, totalUnitsLine];
     return [formattedData];
-  }, [data]);
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box m="1.5rem 2.5rem">
